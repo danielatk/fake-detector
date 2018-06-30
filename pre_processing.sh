bd4d6f80022d098466157e9ce23c9ab271e6715c
Daniel Atkinson Oliveira <danatkhoo@yahoo.com.br>
2018-06-29 22:38:21 -0300

    botar opções de passagem de parâmetros por linha de comando (ex.: -no_stemming)

--- pre_processing.sh
+++ pre_processing.sh
@@ -1,32 +1,44 @@
 #!/bin/bash
 
-path_dataset="" #modifique isso para o caminho onde está o diretório /fake-detector/ (ex: "/home/user/Documents/fake-detector/"
+#receber argumentos por linha de comando (ex: -n=3 (pro n-grama), -no_stemming, -keep_stopwords)
 
-path_code="" #modifique isso para o caminho onde está o diretório que contém os arquivos a serem executadas (por default é o mesmo que path_dataset)
+path_dataset="/home/danielatk/Documents/Trabalho_RI/op_spam_v1.4/" #modifique isso para o caminho onde está o diretório /fake-detector/ (ex: "/home/user/Documents/fake-detector/"
 
-stopwords=$path_code"stopwords.txt"
-vocabulario=$path_code"vocabulario.txt"
+path_code="/home/danielatk/Documents/Trabalho_RI/op_spam_v1.4/" #modifique isso para o caminho onde está o diretório que contém os arquivos a serem executadas (por default é o mesmo que path_dataset)
+
+stopwords=$path_code"stopwords.txt" #guarda a lista de stopwords que será usada para pré-processamento
+vocabulario=$path_code"vocabulario.txt" #guarda o vocabulário total de todas as avaliações
+results=$path_code"results.txt" #guarda resultados da análise dos tipos de palavra nas avaliações
+f_words=$path_code"function_words.txt" #guarda a lista de palavras funcionais que será usada para análise dos tipos de palavra nas avaliações
+adjectives=$path_code"adjectives.txt" #guarda a lista de adjetivos que será usada para análise dos tipos de palavra nas avaliações
+adverbs=$path_code"adverbs.txt" #guarda a lista de advérbios que será usada para análise dos tipos de palavra nas avaliações
+verbs=$path_code"verbs.txt" #guarda a lista de verbos que será usada para análise dos tipos de palavra nas avaliações
+v_nouns=$path_code"verb_and_noun.txt" #guarda a lista de palavras que são tanto verbos quanto substantivos que será usada para análise dos tipos de palavra nas avaliações
 
 gcc -O2 -o $path_code"pre_processing" $path_code"pre_processing.c"
+gcc -O2 -o $path_code"analise" $path_code"analise.c"
 gcc -O3 -o $path_code"stem" $path_code"stem.c"
 
 feature_extract=$path_code"./feature_extraction.py $vocabulario "
+analise=$path_code"./analise $results $f_words $adjectives $adverbs $verbs $v_nouns "
+pre_process=$path_code"./pre_processing $stopwords "
+stem=$path_code"./stem "
 
 for i in $(ls -d $path_dataset*/); do
 	for j in $(ls -d $i*/); do
 		for k in $(ls -d $j*/); do
-			pre_process=$path_code"./pre_processing $stopwords "
-			stem=$path_code"./stem "
 			cd $k
 			for l in $(ls $k); do
 				pre_process="$pre_process$(readlink -f $l) "
 				stem="$stem$(readlink -f $l) "
+				analise="$analise$(readlink -f $l) "
 				feature_extract="$feature_extract$(readlink -f $l) "
 			done
-			$pre_process
-			$stem
 		done
 	done
 done
 
-$feature_extract
+$pre_process #não funciona aqui pois quando renomea saida.txt não está copiando path inteiro do diretorio
+#$stem
+$analise
+#$feature_extract
